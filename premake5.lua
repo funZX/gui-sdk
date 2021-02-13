8b825bf5e3596c12c2fe536569186d12813efb91
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2021-02-14 00:03:23 +0200

    prj

--- premake5.lua
+++ premake5.lua
@@ -8,7 +8,7 @@ if action == "clean" then
 	os.exit(1)
 end
 --------------------------------------------------
-if action == "gmake" then
+if action == "gmake2" then
 	if nil == options["cc"] then
 		print("Choose a C/C++ compiler set.")
 		os.exit(1)
@@ -20,8 +20,11 @@ workspace "gui-sdk"
 	characterset "MBCS"
 	staticruntime "On"
 	omitframepointer "On"
+
+--------------------------------------------------
 	
     configurations { "Debug", "Release" }
+	
 	filter { "configurations:Debug" }
         defines { "DEBUG" }
         symbols "On"
@@ -30,38 +33,37 @@ workspace "gui-sdk"
         defines { "NDEBUG" }
         optimize "On"
 --------------------------------------------------
-filter {"system:windows"}
-	platforms { "x64" }
-	defines { "TARGET_OS_WINDOWS", "WIN32_LEAN_AND_MEAN" }
-	entrypoint "mainCRTStartup"
+	filter {"system:windows"}
+		platforms { "x64" }
+		defines { "TARGET_OS_WINDOWS", "WIN32_LEAN_AND_MEAN" }
+		entrypoint "mainCRTStartup"
 
-filter {"system:linux"}
-	platforms { "x64", "arm32", "arm64" }
-	defines { "TARGET_OS_LINUX" }
-	buildoptions {"-fpermissive"}
+	filter {"system:linux"}
+		platforms { "x64", "arm32", "arm64" }
+		defines { "TARGET_OS_LINUX" }
 
-filter {"platforms:arm*"}	
-	defines { "TARGET_OS_LINUX_ARM" }
+	filter {"platforms:arm*"}	
+		defines { "TARGET_OS_LINUX_ARM" }
 
 --------------------------------------------------	
-filter {"platforms:x64"}
-	architecture "x86_64"
-	
-filter {"platforms:arm32"}
-	architecture "arm"
-	--gccprefix "armv7a-linux-gnueabihf-"
-	
-filter {"platforms:arm64"}
-	architecture "aarch64"
-	--gccprefix "aarch64-linux-gnueabi-"
+	filter {"platforms:x64"}
+		architecture "x86_64"
+		
+	filter {"platforms:arm32"}
+		architecture "arm"
+		--gccprefix "armv7a-linux-gnueabihf-"
+		
+	filter {"platforms:arm64"}
+		architecture "aarch64"
+		--gccprefix "aarch64-linux-gnueabi-"
 --------------------------------------------------
 
-group "libs"
-	include "libs/glfw"
-	include "libs/imgui"
-	include "libs/ofx"
-group ""
+	group "libs"
+		include "libs/glfw"
+		include "libs/imgui"
+		include "libs/ofx"
+	group ""
 
-include("main/premake5.lua")
+	include("main/premake5.lua")
 
 --------------------------------------------------
